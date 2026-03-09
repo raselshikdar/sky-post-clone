50b1aad7fb9864bb12775ea43b00c55ec5a4838a
Rasel Shikdar <imrasel597@gmail.com>
2026-03-09 14:23:25 +0000

    Added dedicated android build workflow

---
+++ android/app/src/main/java/cc/eu/awaj/MainActivity.java
@@ -0,0 +1,5 @@
+package cc.eu.awaj;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cc.eu.awaj',
+  appName: 'Awaj',
+  webDir: 'dist'
+};
+
+export default config;
