6b9e1f69420213c23e36e7c2aa0a953a17910998
Rasel Shikdar <imrasel597@gmail.com>
2026-03-10 12:40:39 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cc.eu.awaj',
   appName: 'Awaj',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https' // এটি যোগ করুন
+  }
 };
 
 export default config;
